1713c563fed4a5fe6e272dbb2d7ea8d193a88fc4
Mark Gotham <19486660+MarkGotham@users.noreply.github.com>
2025-09-30 12:42:04 +0100

    Second method: `score_to_syllables`

--- syllables.py
+++ syllables.py
@@ -24,6 +24,8 @@ as relevant to this meta-corpus (e.g., tinyNotation).
 
 from pathlib import Path
 import re
+from music21 import converter, stream
+from music21.analysis import segmentByRests
 
 
 __author__ = "Mark Gotham"
@@ -55,6 +57,34 @@ def text_to_syllables_lines(
     return all_verses
 
 
+def part_to_syllables_lines(
+    part: stream.Part,
+    min_note_ql: float = 1.0
+) -> list[int]:
+    """
+    Some chorale practices are highly regular in:
+    - having very syllabic settings (no melisma except for the shortest note values)
+    - rests between phrases.
+
+    This basic script applies to such cases, splitting a chorale into phrases,
+    and excluding short notes, returning the number of notes (and probably therefore syllables) per phrase.
+
+    This can be compared with other methods.
+    """
+    syllables_this_verse = []
+
+    segments = segmentByRests.Segmenter.getSegmentsList(part.stripTies())
+    for seg in segments:
+        durs = [x for x in seg if x.duration.quarterLength >= min_note_ql]
+        syllables_this_verse.append(len(durs))
+
+    return syllables_this_verse
+
+
+# -----------------------------------------------------------------------------
+
+# Goudimel-specific
+
 def text_to_syllables_Goudimel(
     psalm_number: int
 ) -> list:
@@ -67,6 +97,29 @@ def text_to_syllables_Goudimel(
     num_padded = str(psalm_number).zfill(3)
     txt_path = Path(".") / "Pseaumes" / num_padded / f"Goudimel_{num_padded}.txt"
     return text_to_syllables_lines(txt_path)
+
+
+def score_to_syllables_Goudimel(
+    psalm_number: int
+) -> list:
+    """
+    A wrapper for Goudimel-specific cases.
+
+    >>> syl = score_to_syllables_Goudimel(3)
+    >>> syl[0]
+    [6, 6, 7, 6, 6, 7, 6, 6, 7, 6, 6, 7]
+
+    >>> syl[1]
+    [6, 6, 7, 6, 6, 7, 6, 6, 7, 6, 6, 7]
+
+    >>> syl[0] == syl[1]
+    True
+
+    """
+    num_padded = str(psalm_number).zfill(3)
+    this_path = Path(".") / "Pseaumes" / num_padded / f"Goudimel_{num_padded}_original.mxl"
+    score = converter.parse(this_path)
+    return [part_to_syllables_lines(p) for p in score.parts]
 
 
 if __name__ == '__main__':
